211cb1be296b4e764f79d1d1d077cf1ee682cc61
János Attila Tapolcsányi <janos.tapolcsanyi@stud.uni-corvinus.hu>
2020-12-07 12:19:28 +0100

    Adatbetöltések

---
+++ IRF_T5IMMU/IRF_T5IMMU/Entities/Adatok.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_T5IMMU.Entities
+{
+    public class Adatok
+    {
+        public int utszam { get; set; } //utazások száma, ezer út
+        public int eltnap { get; set; }//eltöltött napok száma, ezer nap
+        public int koltes { get; set; } //költés, millió Ft
+        public double tartnap { get; set; } //átlagos tartózkodási idő, nap
+        public double napikoltes { get; set; } //egy fő egy napjára jutó költés, ezer Ft
+    }
+}
--- IRF_T5IMMU/IRF_T5IMMU/Form1.cs
+++ IRF_T5IMMU/IRF_T5IMMU/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using IRF_T5IMMU.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,58 @@ namespace IRF_T5IMMU
 {
     public partial class Form1 : Form
     {
+        private List<Adatok> _2019Q3 = new List<Adatok>();
+        private List<Adatok> _2020Q3 = new List<Adatok>();
+
         public Form1()
         {
             InitializeComponent();
+            Adatbetoltes1();
+            Adatbetoltes2();
+        }
+
+        private void Adatbetoltes1()
+        {
+            _2019Q3.Clear();
+
+            using (StreamReader sr = new StreamReader("2019_Q3.csv", Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+
+                    Adatok a = new Adatok();
+                    a.utszam = int.Parse(line[0]);
+                    a.eltnap = int.Parse(line[1]);
+                    a.koltes = int.Parse(line[2]);
+                    a.tartnap = double.Parse(line[3]);
+                    a.napikoltes = double.Parse(line[4]);
+                    _2019Q3.Add(a);
+                }
+            }
+        }
+
+        private void Adatbetoltes2()
+        {
+            _2020Q3.Clear();
+
+            using (StreamReader sr = new StreamReader("2020_Q3.csv", Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+
+                    Adatok a = new Adatok();
+                    a.utszam = int.Parse(line[0]);
+                    a.eltnap = int.Parse(line[1]);
+                    a.koltes = int.Parse(line[2]);
+                    a.tartnap = double.Parse(line[3]);
+                    a.napikoltes = double.Parse(line[4]);
+                    _2020Q3.Add(a);
+                }
+            }
+
         }
     }
+
 }
