20ae98cfc7ca33a8e4f30b45c3d96ad1385190c7
János Attila Tapolcsányi <janos.tapolcsanyi@stud.uni-corvinus.hu>
2020-12-09 12:40:53 +0100

    Excel generálás és formázás

--- IRF_T5IMMU/IRF_T5IMMU/Form2.cs
+++ IRF_T5IMMU/IRF_T5IMMU/Form2.cs
@@ -9,6 +9,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+
 
 namespace IRF_T5IMMU
 {
@@ -19,6 +23,11 @@ namespace IRF_T5IMMU
         string[] fejlecek = new string[6];
         BindingList<Adatok> _2019Q3 = new BindingList<Adatok>();
         BindingList<Adatok> _2020Q3 = new BindingList<Adatok>();
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+        
         public Form2()
         {
             InitializeComponent();
@@ -79,6 +88,102 @@ namespace IRF_T5IMMU
                          select i).ToList();
                 dataGridView2.DataSource = v;
             }
+        }
+
+        void CreateExcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        void CreateTable()
+        {
+            xlSheet.Name = "2019Q3";
+            string[] headers = new string[] {
+                "Országok",
+                "Utazások száma, ezer út",
+                "Eltöltött napok száma, ezer nap",
+                "Költés, millió Ft",
+                "Átlagos tartózkodási idő, nap",
+                "Egy fő egy napjára jutó költés, ezer Ft"
+            };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, (i + 1)] = headers[i];
+                //MessageBox.Show(headers[i]);
+            }
+
+            object[,] values = new object[_2019Q3.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Adatok a in _2019Q3)
+            {
+                values[counter, 0] = a.orszag;
+                values[counter, 1] = a.utszam;
+                values[counter, 2] = a.eltnap;
+                values[counter, 3] = a.koltes;
+                values[counter, 4] = a.tartnap;
+                values[counter, 5] = a.napikoltes;
+                counter++;
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            xlSheet.Cells[2 + values.GetLength(0), 1] = "Összesen:";
+            xlSheet.Cells[2 + values.GetLength(0), 2] = ("=SZUM(B2:B"+(1 + values.GetLength(0))+")") ;
+            xlSheet.Cells[2 + values.GetLength(0), 3] = ("=SZUM(C2:C" + (1 + values.GetLength(0)) + ")");
+            xlSheet.Cells[2 + values.GetLength(0), 4] = ("=SZUM(D2:D" + (1 + values.GetLength(0)) + ")");
+            xlSheet.Cells[2 + values.GetLength(0), 5] = ("=ÁTLAG(E2:E" + (1 + values.GetLength(0)) + ")");
+            xlSheet.Cells[2 + values.GetLength(0), 6] = "=D" + (2 + values.GetLength(0)) + "/C" + (2 + values.GetLength(0));
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.WrapText = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignBottom;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 60;
+            headerRange.Interior.Color = Color.Yellow;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThin);
+            
+
+            Excel.Range orszagRange = xlSheet.get_Range(GetCell(2, 1), GetCell(2 + values.GetLength(0),1));
+            orszagRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignRight;
+            orszagRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlHairline);
+
+            Excel.Range osszesenRange = xlSheet.get_Range(GetCell(2 + values.GetLength(0), 1), GetCell(2 + values.GetLength(0), headers.Length));
+            osszesenRange.Font.Bold = true;
+            osszesenRange.Font.Color = Color.Red;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -144,6 +249,28 @@ namespace IRF_T5IMMU
         private void Form2_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CreateExcel();
         }
     }
 }
